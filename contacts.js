6f57b6442f887335435bbbf76bada4b747bae31c
Haidai Kostiantyn <thekos0596@gmail.com>
2023-06-15 21:37:36 +0200

    update

--- contacts.js
+++ contacts.js
@@ -5,22 +5,22 @@ const contactsPath = path.basename("./db/contacts.json");
 
 // TODO: задокументировать каждую функцию
 function listContacts() {
-  fs.readFile({ contactsPath })
-    .then((data) => console.log(JSON.parse(data)))
+  fs.readFile(contactsPath)
+    .then((data) => console.log(hello))
     .catch((err) => console.log(err.message));
 }
 
-function getContactById(contactId) {
-  // ...твой код
-}
+// function getContactById(contactId) {
+//   // ...твой код
+// }
 
-function removeContact(contactId) {
-  // ...твой код
-}
+// function removeContact(contactId) {
+//   // ...твой код
+// }
 
-function addContact(name, email, phone) {
-  // ...твой код
-}
+// function addContact(name, email, phone) {
+//   // ...твой код
+// }
 
 module.exports = {
   listContacts,
